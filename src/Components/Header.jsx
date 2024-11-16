07bbddb2469c5dd4702f5d9e417bcd9c8a56b1c0
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-11-16 22:53:10 +0100

    changes made

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -48,8 +48,8 @@ export default function Header() {
             <div className="hidde md:block xl:max-w-[90%] mx-auto">
                 <div className=" nav flex md:gap-7 gap-4 font-medium justify-center items-center text-[15px] nav">
                     <Link to={'/'} className=' text-white link hover:text-blue-500 duration-200'>Home</Link>
-                    <Link to={'/news'} className=' text-white link hover:text-blue-500 duration-200'>News</Link>
-                    <Link to={'/gadgets'} className=' text-white link hover:text-blue-500 duration-200'>Gadgets</Link>
+                    <Link to='/news' className=' text-white link hover:text-blue-500 duration-200'>News</Link>
+                    <Link to='/gadgets' className=' text-white link hover:text-blue-500 duration-200'>Gadgets</Link>
                     <Link to={'/bookings'} className=' text-white link hover:text-blue-500 duration-200'>Booking Codes</Link>
                 </div>
             </div>
