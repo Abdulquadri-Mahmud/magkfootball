3f1e05ac38f4408411771ac1140db04f17a3a893
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-11-16 22:15:56 +0100

    changes made

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,11 @@ import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 // import { ChakraProvider } from '@chakra-ui/react'
 import { persistor, store } from './store/index.js'
-import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </PersistGate>
   </Provider>
 )
