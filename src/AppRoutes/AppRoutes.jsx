6a484858db98a85497481714723dd84bc7a32869
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-11-16 22:22:21 +0100

    changes made

--- src/AppRoutes/AppRoutes.jsx
+++ src/AppRoutes/AppRoutes.jsx
@@ -20,7 +20,7 @@ import PageNotFound from '../PgaeNotFound/PageNotFound';
 export default function AppRoutes() {
   return (
     <div>
-        <Router basename="/https://magkfootball.vercel.app/">
+        <Router basename="/">
           <Header/>
             <Routes>
                 <Route path='/' element={<Home/>}/>
